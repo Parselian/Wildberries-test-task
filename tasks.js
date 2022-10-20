56a12c75fdd90f2adef441c8ace3d1422b1fb885
Parselian <parselian.study@yandex.ru>
2022-10-20 09:31:28 +0300

    make array.find() function

--- tasks.js
+++ tasks.js
@@ -1,10 +1,21 @@
 'use-strict'
 
+const temporaryData = [{a: 10, b: true}, {a:15, b: false}, {a:10, b: false}]
+
 const api = {
   filterArray: (array) => {
       return [...new Set(array)]
+  },
+  filterObj: (arrayOfObjects, callback) => {
+    for (let counter = 0; counter < arrayOfObjects.length; counter++) {
+      if (callback(arrayOfObjects[counter])) {
+        return arrayOfObjects[counter]
+      }
+    }
   }
 }
 
 
-console.log(api.filterArray(['Яблоко','Яблоко', 'banana', 1, 1, 2]))
+console.log( api.filterArray(['Яблоко','Яблоко', 'banana', 1, 1, 2]) )
+
+console.log( api.filterObj(temporaryData, (el) => el.a > 10 && el.b === false ) )
